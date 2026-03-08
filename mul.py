9b76c4ad8b3e73ab99388c04a067ba858816e83f
Minh Nguyen <99856380+bogaktik@users.noreply.github.com>
2026-03-07 21:23:58 -0500

    Integrate RGQFTMultiplier and add measurements
    Added RGQFTMultiplier circuit and measurement steps to the quantum circuit.

--- mul.py
+++ mul.py
@@ -10,3 +10,21 @@ circuit = QuantumCircuit(q, c) # create a quantum circuit
 
 circuit.h(q[0:3]) # apply hadamard gates to the first 3 qubits
 circuit.h(q[3:6]) # apply hadamard gates to the next 3 qubits
+
+circuit1 = RGQFTMultiplier(num_state_qubits=3, num_result_qubits=6)
+circuit = circuit.compose(circuit1)
+
+circuit.measure(q[7],c[0])
+circuit.measure(q[8],c[1])
+circuit.measure(q[9],c[2])
+circuit.measure(q[10],c[3])
+circuit.measure(q[11],c[4])
+circuit.measure(q[12],c[5])
+
+print(circuit)
+
+job = execute(circuit, backend, shots=1024)
+result = job.result()
+counts = result.get_counts()
+
+print(counts)
